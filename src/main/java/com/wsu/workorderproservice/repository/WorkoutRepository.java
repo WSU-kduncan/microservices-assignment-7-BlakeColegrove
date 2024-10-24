eb91ae34ed5c66e34a1e4dd714bd02438c387d81
Blake <colegrove.5@wright.edu>
2024-10-23 22:01:14 -0400

    all repos written

--- src/main/java/com/wsu/workorderproservice/repository/WorkoutRepository.java
+++ src/main/java/com/wsu/workorderproservice/repository/WorkoutRepository.java
@@ -1,5 +1,19 @@
 package com.wsu.workorderproservice.repository;
 
-public class WorkoutRepository {
+import com.wsu.workorderproservice.model.Workout;
+
+import jakarta.validation.constraints.Null;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface WorkoutRepository extends JpaRepository<Workout, Integer> {
+
+    @Query(nativeQuery = true, value = "") // I really dont know how to write the query, this is something I need help with 
+    Page<Object[]> findBySearch(String search, Pageable pageable);
+
+    boolean existsById(Integer id);
 
 }
