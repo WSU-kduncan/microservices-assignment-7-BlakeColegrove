3c0f35772bb6d479ea44f28cf37f69cf4ff8e497
Blake <colegrove.5@wright.edu>
2024-10-23 21:54:42 -0400

    all models created

--- src/main/java/com/wsu/workorderproservice/model/Runner.java
+++ src/main/java/com/wsu/workorderproservice/model/Runner.java
@@ -1,5 +1,46 @@
 package com.wsu.workorderproservice.model;
 
-public class Runner {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.criteria.CriteriaBuilder.In;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@Table(name = "runner")
+public class Runner {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "runner_id")
+    private Integer id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "gender")
+    private String gender;
+
+    @Column(name = "height")
+    private Integer height;
+
+    @Column(name = "age")
+    private Integer age;
+
+    @Column(name = "weight")
+    private Integer weight;
 }
