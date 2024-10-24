81b61ed1aa123ddf2bb9e5c8fbcd480066a89d1d
Blake <colegrove.5@wright.edu>
2024-10-23 21:28:32 -0400

    all controller classes done

---
+++ src/main/java/com/wsu/workorderproservice/controller/EvaluationController.java
@@ -0,0 +1,55 @@
+package com.wsu.workorderproservice.controller;
+
+import org.hibernate.query.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wsu.workorderproservice.dto.ServiceResponseDTO;
+import com.wsu.workorderproservice.service.EvaluationService;
+import com.wsu.workorderproservice.utilities.Constants;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping
+@RequiredArgsConstructor
+public class EvaluationController {
+
+    private final EvaluationService evaluationService;
+
+    @GetMapping
+    public ResponseEntity<ServiceResponseDTO> getEvaluations() {
+        // this will take the params to format the page results like normal
+        // it will return an HTTP status ok message and return all evaluations for a given
+        // Runner based on the Runners ID/email (deciding about using email based on in class discussion)
+    }     
+    
+    @PostMapping
+    public ResponseEntity<ServiceResponseDTO> createEvaluation() {
+        // this will take in an @PathVariable (probably the Runners ID) and create
+        // an evaluation or the Runner and matched workout
+    }
+
+    @PutMapping("/{runnerId}")
+    public ResponseEntity<ServiceResponseDTO> updateEvaluation() {
+        // there will be a try-catch block here that will have conditions to handle
+        // a successful update, invalid data, no matching evaluation, and any random exception.
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ServiceResponseDTO> deleteEvaluation() {
+        // deletes an evaluation based on a given evaluationId
+        // exception thrown on evaluation DNE
+    }
+
+    // Down here there will be handleErrorResponse methods to assist in handling the exceptions
+    // within the above methods.
+    // This is used for HTTP status 400 and 500 codes.
+}
--- src/main/java/com/wsu/workorderproservice/controller/WorkoutController.java
+++ src/main/java/com/wsu/workorderproservice/controller/WorkoutController.java
@@ -1,5 +1,61 @@
 package com.wsu.workorderproservice.controller;
 
+import org.hibernate.query.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wsu.workorderproservice.dto.ServiceResponseDTO;
+import com.wsu.workorderproservice.service.EvaluationService;
+import com.wsu.workorderproservice.service.RunnerService;
+import com.wsu.workorderproservice.service.WorkoutService;
+import com.wsu.workorderproservice.utilities.Constants;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping
+@RequiredArgsConstructor
 public class WorkoutController {
 
+    private final WorkoutService workoutService;
+
+    @GetMapping
+    public ResponseEntity<ServiceResponseDTO> getWorkout() {
+        // this will take the params to format the page results like normal
+        // it will return an HTTP status ok message and return the data for a given workout
+        // based on the provided runnerId. 
+        // This would be in the case that a runner is looking up all their workouts.
+        // An optional param could be an evaluationId or details within an evaluation to pull up a specific or 
+        // filtered to a specific type of workout
+    }     
+    
+    @PostMapping
+    public ResponseEntity<ServiceResponseDTO> createWorkout() {
+        // creates a workout and generates a workoutId to correspond
+        // All params are REQUIRED because it is the creation of a workout.
+    }
+
+    @PutMapping("/{runnerId}")
+    public ResponseEntity<ServiceResponseDTO> updateWorkout() {
+        // there will be a try-catch block here that will have conditions to handle
+        // a successful update, invalid data, no matching workout, and any random exception.
+        // This would if a user is adjusting the specifics of an existing saved workout. Values cannot be NULL.
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ServiceResponseDTO> deleteWorkout() {
+        // deletes a workout based on a given workoutId
+        // exception thrown on workout DNE
+    }
+
+    // Down here there will be handleErrorResponse methods to assist in handling the exceptions
+    // within the above methods.
+    // This is used for HTTP status 400 and 500 codes.
 }
